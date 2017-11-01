ccd47feb673f218ac00c8878239bd3943ee9cb71
Vick Vasquez <vasjvic44@gmail.com>
2017-11-01 03:58:34 -0600

    Se crea interface para setear datos en los modelos (POJO)

--- app/src/main/java/com/programandoando/vick/retrofitapirest/MainActivity.java
+++ app/src/main/java/com/programandoando/vick/retrofitapirest/MainActivity.java
@@ -19,6 +19,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.programandoando.vick.retrofitapirest.postsApi.data.model.Comments;
+import com.programandoando.vick.retrofitapirest.postsApi.data.model.CommentsPosts;
 import com.programandoando.vick.retrofitapirest.postsApi.data.model.Posts;
 import com.programandoando.vick.retrofitapirest.postsApi.data.api.retrofit.RestApiAdapter;
 import com.programandoando.vick.retrofitapirest.postsApi.data.api.client.Service;
@@ -48,10 +49,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        getUsers();
+        //getUsers();
         //getSinglePost();
-        //getPosts();
+        getCommentsPost();
         //getComments();
+        //getPosts();
     }
 
     @Override
@@ -76,111 +78,131 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void getUsers() {
+    private void getCommentsPost() {
+
         RestApiAdapter restApiAdapter = new RestApiAdapter();
 
-        Service service = restApiAdapter.getClientService();
+        restApiAdapter
+                .getClientService()
+                .getPosts(1)
+                .enqueue(new Callback<List<CommentsPosts>>() {
+                    @Override
+                    public void onResponse(Call<List<CommentsPosts>> call, Response<List<CommentsPosts>> response) {
+                        Log.i(TAG,"Nu. de comentarios recuperados del post 1 => "+ response.body().size());
+                    }
 
-        Call<JsonArray> users = service.getUsers();
+                    @Override
+                    public void onFailure(Call<List<CommentsPosts>> call, Throwable t) {
 
-        users.enqueue(new Callback<JsonArray>() {
-            @Override
-            public void onResponse(Call<JsonArray> call, Response<JsonArray> response) {
-                JsonArray usersResponse = response.body().getAsJsonArray();
+                    }
+                });
 
-                Gson gson = new Gson();
+    }
 
-                Users usersData[] = gson.fromJson(usersResponse.toString(),Users[].class);
+    private void getUsers() {
+        RestApiAdapter restApiAdapter = new RestApiAdapter();
+        restApiAdapter
+                .getClientService()
+                .getUsers()
+                .enqueue(new Callback<List<Users>>() {
+                    @Override
+                    public void onResponse(Call<List<Users>> call, Response<List<Users>> response) {
+        /*
+                        JsonArray usersResponse = response.body().getAsJsonArray();
 
-                List<Users> userModel = Arrays.asList(usersData);
+                        Gson gson = new Gson();
 
-                Log.w(TAG,"Usuario 1 "+ userModel.get(0).toString());
-            }
+                        Users usersData[] = gson.fromJson(usersResponse.toString(),Users[].class);
 
-            @Override
-            public void onFailure(Call<JsonArray> call, Throwable t) {
+                        List<Users> userModel = Arrays.asList(usersData);
+        */
 
-            }
-        });
+                        Log.w(TAG,"Usuario 1 "+ response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Users>> call, Throwable t) {
+
+                    }
+                });
     }
 
     private void getSinglePost() {
         RestApiAdapter restApiAdapter = new RestApiAdapter();
+        restApiAdapter
+                .getClientService()
+                .getPostSingle(2)
+                .enqueue(new Callback<SinglePost>() {
+                    @Override
+                    public void onResponse(Call<SinglePost> call, Response<SinglePost> response) {
+                        /*
+                        JsonObject dataSinglePost = response.body().getAsJsonObject();
 
-        Service service = restApiAdapter.getClientService();
+                        Gson gson = new Gson();
+                        SinglePost singlePostData = gson.fromJson(dataSinglePost.toString(),SinglePost.class);*/
 
-        Call<JsonObject> postSingle = service.getPostSingle(2);
+                        String singlePost = response.body().toString();
 
-        postSingle.enqueue(new Callback<JsonObject>() {
-            @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                JsonObject dataSinglePost = response.body().getAsJsonObject();
+                        Log.w(TAG,"Post 2 => "+ singlePost);
 
-                Gson gson = new Gson();
-                SinglePost singlePostData = gson.fromJson(dataSinglePost.toString(),SinglePost.class);
-                Log.w(TAG,"Post 2 => "+ singlePostData.toString());
+                    }
 
-            }
+                    @Override
+                    public void onFailure(Call<SinglePost> call, Throwable t) {
 
-            @Override
-            public void onFailure(Call<JsonObject> call, Throwable t) {
-
-            }
-        });
+                    }
+                });
     }
 
     private void getComments() {
 
         RestApiAdapter restApiAdapter = new RestApiAdapter();
+        restApiAdapter
+                    .getClientService()
+                    .getComments()
+                    .enqueue(new Callback<List<Comments>>() {
+                        @Override
+                        public void onResponse(Call<List<Comments>> call, Response<List<Comments>> response) {
+                            String comment = response.body().get(0).toString();
+                            Log.w(TAG,"Comentario uno "+ comment);
+                            /*
+                            Gson gson = new Gson();
 
-        Service service = restApiAdapter.getClientService();
+                            Comments[] comment = gson.fromJson(comments.toString(),Comments[].class);
+                            List<Comments> modelComments = Arrays.asList(comment);
 
-        Call<JsonArray> call = service.getComments();
+                            Log.w(TAG,"Comentario uno "+ modelComments.get(0).toString());
+                            */
+                        }
 
-        call.enqueue(new Callback<JsonArray>() {
-            @Override
-            public void onResponse(Call<JsonArray> call, Response<JsonArray> response) {
-                JsonArray comments = response.body().getAsJsonArray();
+                        @Override
+                        public void onFailure(Call<List<Comments>> call, Throwable t) {
 
-                Gson gson = new Gson();
-
-                Comments[] comment = gson.fromJson(comments.toString(),Comments[].class);
-                List<Comments> modelComments = Arrays.asList(comment);
-
-                Log.w(TAG,"Comentario uno "+ modelComments.get(0).toString());
-            }
-
-            @Override
-            public void onFailure(Call<JsonArray> call, Throwable t) {
-
-            }
-        });
+                        }
+                    });
     }
 
     private void getPosts()  {
 
         RestApiAdapter restApiAdapter = new RestApiAdapter();
-
+  /*
         Service service = restApiAdapter.getClientService();
 
-        Call<JsonArray> call = service.getDataPosts();
+        Call<List<Posts>> call = service.getDataPosts();
+*/
+        restApiAdapter
+                .getClientService()
+                .getDataPosts()
+                .enqueue(new Callback<List<Posts>>() {
+                    @Override
+                    public void onResponse(Call<List<Posts>> call, Response<List<Posts>> response) {
+                        Log.w(TAG," Respuestas => "+ response.body());
+                    }
 
-        call.enqueue(new Callback<JsonArray>() {
-            @Override
-            public void onResponse(Call<JsonArray> postListcall, Response<JsonArray> response) {
-
-                JsonArray posts = response.body().getAsJsonArray();
-                Gson gson = new Gson();
-                Posts[] post = gson.fromJson(posts.toString(), Posts[].class);
-                List<Posts> modelPost =  Arrays.asList(post);
-
-                Log.w(TAG, "onResponse: " + modelPost.get(0).toString() );
-            }
-
-            @Override
-            public void onFailure(Call<JsonArray> call, Throwable t) {
-                Log.w(TAG,"Posts falsos "+ t.toString() );
-            }
-        });
+                    @Override
+                    public void onFailure(Call<List<Posts>> call, Throwable t) {
+                        Log.e(TAG,"Error => "+ t.toString());
+                    }
+                });
     }
 }
--- app/src/main/java/com/programandoando/vick/retrofitapirest/postsApi/data/api/client/Service.java
+++ app/src/main/java/com/programandoando/vick/retrofitapirest/postsApi/data/api/client/Service.java
@@ -3,6 +3,13 @@ package com.programandoando.vick.retrofitapirest.postsApi.data.api.client;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.programandoando.vick.retrofitapirest.postsApi.data.api.Constants;
+import com.programandoando.vick.retrofitapirest.postsApi.data.model.Comments;
+import com.programandoando.vick.retrofitapirest.postsApi.data.model.CommentsPosts;
+import com.programandoando.vick.retrofitapirest.postsApi.data.model.Posts;
+import com.programandoando.vick.retrofitapirest.postsApi.data.model.SinglePost;
+import com.programandoando.vick.retrofitapirest.postsApi.data.model.Users;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -15,17 +22,17 @@ import retrofit2.http.Query;
 
 public interface Service {
     @GET(Constants.POSTS)
-    Call<JsonArray> getDataPosts();
+    Call<List<Posts>> getDataPosts();
 
     @GET(Constants.COMMENTS)
-    Call<JsonArray> getComments();
+    Call<List<Comments>> getComments();
 
     @GET(Constants.USERS)
-    Call<JsonArray> getUsers();
+    Call<List<Users>> getUsers();
 
     @GET(Constants.POSTS+"/{idPost}")
-    Call<JsonObject> getPostSingle(@Path("idPost") Integer idPost);
+    Call<SinglePost> getPostSingle(@Path("idPost") Integer idPost);
 
-    @GET(Constants.COMMENTS+"/?postId={idPost}")
-    Call<JsonArray> getPosts(@Query("idPost") Integer idPost);
+    @GET(Constants.COMMENTS+"/")
+    Call<List<CommentsPosts>> getPosts(@Query("postId") Integer postId);
 }
